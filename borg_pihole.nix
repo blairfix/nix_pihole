4c177e2d9a1d78ff220628950167a0a6d09b3ca7
blairfix <blairfix@fastmail.com>
2025-02-08 16:48:09 -0700

    borg

---
+++ borg_pihole.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+{
+
+    # borg pihole
+    #----------------------------------------
+
+    systemd.timers."borg_pihole" = {
+	wantedBy = [ "timers.target" ];
+	timerConfig = {
+	    OnCalendar = "*-*-*  *:19:00";
+	    Persistent = "true";
+	    Unit = "borg_pihole.service";
+	};
+    };
+    in {
+
+	systemd.services."borg_pihole" = {
+	    serviceConfig = {
+		Type = "simple";
+		User = "pihole";
+	    };
+	    path = with pkgs; [ 
+		bash
+		borgbackup
+	    ];
+	    script = ''
+		bash /home/blair/bin/backup.sh
+		'';
+	};
+
+    }
+
